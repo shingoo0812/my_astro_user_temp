9ad7cbca5d3f27e58b38d2dd75d7680a4e537dda
shingoo0812 <shingoo0812@gmail.com>
2024-06-25 08:14:23 +0900

    modify nvim

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -30,6 +30,8 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        tabstop = 4,
+        shiftwidth = 4,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
